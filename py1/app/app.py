b50a388afa225ddc886db29bcddb6a5e7c1a10a3
D-Vinokurov221-351 <dengaming2005@gmail.com>
2024-03-31 10:42:42 +0300

    2 lb

--- py1/app/app.py
+++ py1/app/app.py
@@ -1,6 +1,8 @@
 import random
 from flask import Flask, render_template
 from faker import Faker
+from flask import request
+from flask import make_response
 
 fake = Faker()
 
@@ -38,6 +40,10 @@ posts_list = sorted([generate_post(i) for i in range(5)], key=lambda p: p['date'
 def index():
     return render_template('index.html')
 
+@app.route('/url')
+def url():
+    return render_template('url.html', title="Параметры URL", )
+
 @app.route('/posts')
 def posts():
     return render_template('posts.html', title='Посты', posts=posts_list)
@@ -50,3 +56,49 @@ def post(index):
 @app.route('/about')
 def about():
     return render_template('about.html', title='Об авторе')
+
+@app.route('/forms', methods=['GET', 'POST'])
+def forms():
+    # if request.method == "POST"
+    return render_template('forms.html', title="Параметры формы")
+
+@app.route('/headers')
+def headers():
+    return render_template('headers.html', title="Заголовки")
+
+
+@app.route('/cookies')
+def cookies():
+    resp = make_response(render_template('cookies.html', title="Куки"))
+    if 'user' in request.cookies:
+        resp.delete_cookie('user')
+    else:
+        resp.set_cookie('user', 'admin')
+    return resp
+
+@app.route("/phoneNumber", methods=["POST", "GET"])
+def phoneNumber():
+    if request.method == 'POST':
+        phone = request.form["phone"]
+
+        phoneNumbers = re.findall("\d{1}", phone)
+        if not phoneNumbers:
+            phoneNumbers.append("")
+
+        error = ""
+        if not all([symbol in [" ", "(", ")", "-", ".", "+", *list(map(str, list(range(10))))] for symbol in phone]):
+            error = "Недопустимый ввод. В номере телефона встречаются недопустимые символы."
+        elif phoneNumbers[0] in ["7", "8"] and len(phoneNumbers) != 11:
+            error = "Недопустимый ввод. Неверное количество цифр."
+        elif phoneNumbers[0] not in ["7", "8"] and len(phoneNumbers) != 10:
+            error = "Недопустимый ввод. Неверное количество цифр."
+
+        if error:
+            return render_template("phoneNumber.html", title="Проверка номера телефона", phone=error)
+
+        if len(phoneNumbers) == 10:
+            phoneNumbers.insert(0, "8")
+
+        return render_template("phoneNumber.html", title="Проверка номера телефона", phone="8-{1}{2}{3}-{4}{5}{6}-{7}{8}-{9}{10}".format(*phoneNumbers))
+    else:
+        return render_template("phoneNumber.html", title="Проверка номера телефона")
